fa8642f96066680debd377813323c6a1eda2cf62
Taylan Santos <102376714+Taylanbs@users.noreply.github.com>
2026-07-17 23:02:09 -0300

    Add initial configuration settings in config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// ════════════════════════════════════════════════
+//  LANY · CONFIG
+// ════════════════════════════════════════════════
+
+var CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzZ9RZobY4GGeFI1oSoI6rqv1TCv9j3HnzZElvaB4TcqlfkF_fr2awKOaFaiq2v9NTp/exec',
+  DASH_PASSWORD: 'lany2025'
+};
